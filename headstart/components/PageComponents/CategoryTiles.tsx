f62f9fadbd7906aea187510865581c54cb981a53
Bhavesh Rana <brana@horizontalintegration.com>
2023-04-01 19:00:19 +0530

    API for Category

--- headstart/components/PageComponents/CategoryTiles.tsx
+++ headstart/components/PageComponents/CategoryTiles.tsx
@@ -1,68 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from "next/link";
 import Image from 'next/image';
+import categoryHandler from '@/pages/api/category';
+import CategoryCard from './category-card';
 
 export default function CategoryTiles(){
+    const [catagory, setCatagory] = useState([]);
+    useEffect(() =>{
+        fetch('http://localhost:3000/api/category')
+        .then(response => response.json())
+        .then(res => setCatagory(res))
+    }, [])
     return(
         <>
-        <div className="container-fluid pt-5">
-                <div className="row px-xl-5 pb-3">
-                    <div className="col-lg-4 col-md-6 pb-1">
-                        <div className="cat-item d-flex flex-column border mb-4" style={{padding: "30px;"}}>
-                            <p className="text-right">15 Products</p>
-                            <Link href={""} className="cat-img position-relative overflow-hidden mb-3">
-                                <Image src={"/img/cat-1.jpg"} alt='' className="img-fluid" width={500} height={400}></Image>
-                            </Link>                    
-                            <h5 className="font-weight-semi-bold m-0">Men's dresses</h5>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 pb-1">
-                        <div className="cat-item d-flex flex-column border mb-4" style={{padding: "30px;"}}>
-                            <p className="text-right">15 Products</p>
-                            <Link href={""} className="cat-img position-relative overflow-hidden mb-3">
-                                <Image src={"/img/cat-2.jpg"} alt='' className="img-fluid" width={500} height={400}></Image>
-                            </Link>
-                            <h5 className="font-weight-semi-bold m-0">Women's dresses</h5>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 pb-1">
-                        <div className="cat-item d-flex flex-column border mb-4" style={{padding: "30px;"}}>
-                            <p className="text-right">15 Products</p>
-                            <Link href={""} className="cat-img position-relative overflow-hidden mb-3">
-                                <Image src={"/img/cat-3.jpg"} alt='' className="img-fluid" width={500} height={400}></Image>
-                            </Link>
-                            <h5 className="font-weight-semi-bold m-0">Baby's dresses</h5>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 pb-1">
-                        <div className="cat-item d-flex flex-column border mb-4" style={{padding: "30px;"}}>
-                            <p className="text-right">15 Products</p>
-                            <Link href={""} className="cat-img position-relative overflow-hidden mb-3">
-                                <Image src={"/img/cat-4.jpg"} alt='' className="img-fluid" width={500} height={400}></Image>
-                            </Link>
-                            <h5 className="font-weight-semi-bold m-0">Accerssories</h5>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 pb-1">
-                        <div className="cat-item d-flex flex-column border mb-4" style={{padding: "30px;"}}>
-                            <p className="text-right">15 Products</p>
-                            <Link href={""} className="cat-img position-relative overflow-hidden mb-3">
-                                <Image src={"/img/cat-5.jpg"} alt='' className="img-fluid" width={500} height={400}></Image>
-                            </Link>
-                            <h5 className="font-weight-semi-bold m-0">Bags</h5>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 pb-1">
-                        <div className="cat-item d-flex flex-column border mb-4" style={{padding: "30px;"}}>
-                            <p className="text-right">15 Products</p>
-                            <Link href={""} className="cat-img position-relative overflow-hidden mb-3">
-                                <Image src={"/img/cat-6.jpg"} alt='' className="img-fluid" width={500} height={400}></Image>
-                            </Link>
-                            <h5 className="font-weight-semi-bold m-0">Shoes</h5>
-                        </div>
-                    </div>
+        <div className="container-fluid pt-5">                
+                <div className="row px-xl-5 pb-3">  
+                    {catagory.slice(2,8).map((cat) => ( 
+                        <div className="col-lg-4 col-md-6 pb-1">
+                        <div className="cat-item d-flex flex-column border mb-4" style={{padding: "30px"}}>                       
+                            <CategoryCard category={cat} />       
+                            </div>
+                        </div>                 
+                    ))}                                                                              
                 </div>
-            </div>
+            </div>            
         </>
     )
 }
