0e3e149413af2823c65ed4456dd70d17454db58a
Alok Kadudeshmukh <akadudeshmukh@horizontal.com>
2023-04-02 13:22:23 +0530

    email subscription without success

--- headstart/components/PageComponents/EmailSubscription.tsx
+++ headstart/components/PageComponents/EmailSubscription.tsx
@@ -1,6 +1,31 @@
 
 
 const Subscription = () =>{
+    //handle form submit event
+    const handleSubmit =async (event:any)=>{
+        console.log("Handle Submit");
+        //stop refreshing the page on submit
+        event.preventDefault()
+        const data={
+            email:event.target.email.value
+        }
+        const jsonResult=JSON.stringify(data);
+
+        const endPoint='https://sample-apis.azurewebsites.net/api/open/subscribe';
+        const options={
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json',
+            },
+            body:jsonResult
+        }
+
+        //submit the form to API
+        const resp=await fetch(endPoint,options);
+        console.log(resp);
+        resp.status==201?console.log("success"):'';
+
+    }
     return(
         <>
     
@@ -11,13 +36,13 @@ const Subscription = () =>{
                     <h2 className="section-title px-5 mb-3"><span className="bg-secondary px-2">Stay Updated</span></h2>
                     <p>Amet lorem at rebum amet dolores. Elitr lorem dolor sed amet diam labore at justo ipsum eirmod duo labore labore.</p>
                 </div>
-                <form action="" method="post">
+                <form onSubmit={handleSubmit} >
                     <div className="input-group">
                     <label htmlFor="email"></label>
                     <input type="text" required id="email" name="email" className="form-control border-white p-4" placeholder="Email Goes Here" />
                         
                         <div className="input-group-append">
-                            <button className="btn btn-primary px-4">Subscribe</button>
+                            <button className="btn btn-primary px-4"  type="submit">Subscribe</button>
                         </div>
                     </div>
                 </form>
