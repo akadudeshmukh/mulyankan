c84a6845e3eb4f2e9778170d3aea65aff9541dd9
Alok Kadudeshmukh <akadudeshmukh@horizontal.com>
2023-04-02 16:07:41 +0530

    emailSubscriptionwith

--- headstart/components/PageComponents/EmailSubscription.tsx
+++ headstart/components/PageComponents/EmailSubscription.tsx
@@ -1,9 +1,10 @@
+import { useState } from "react";
 
 
-const Subscription = () =>{
+//const SubscriptionForm = ({success}:{success:()=>void}) =>{
+const SubscriptionForm = ({success}:{success:(email:string)=>void}) =>{   
     //handle form submit event
-    const handleSubmit =async (event:any)=>{
-        console.log("Handle Submit");
+    const handleSubmit =async (event:any)=>{        
         //stop refreshing the page on submit
         event.preventDefault()
         const data={
@@ -21,10 +22,8 @@ const Subscription = () =>{
         }
 
         //submit the form to API
-        const resp=await fetch(endPoint,options);
-        console.log(resp);
-        resp.status==201?console.log("success"):'';
-
+        const resp=await fetch(endPoint,options);      
+        resp.status==201?success(event.target.email.value):'';
     }
     return(
         <>
@@ -45,7 +44,7 @@ const Subscription = () =>{
                             <button className="btn btn-primary px-4"  type="submit">Subscribe</button>
                         </div>
                     </div>
-                </form>
+                </form>               
             </div>
         </div>
     </div>
@@ -53,4 +52,38 @@ const Subscription = () =>{
         </>
     )
 }
+
+
+const Subscription = ()=>{
+    const [formSubmitted,setFormSubmitted]=useState(false)
+    const [emailadd,setemailadd]=useState("")
+    return(
+        <>
+        {/* {!formSubmitted && <SubscriptionForm success={()=>setFormSubmitted(true)}></SubscriptionForm> } */}
+        {!formSubmitted  && <SubscriptionForm success={(email:string)=>{
+            setFormSubmitted(true)
+            setemailadd(email)
+        }}></SubscriptionForm> }
+        {formSubmitted &&
+        
+            <div className="container-fluid bg-secondary my-5">
+            <div className="row justify-content-md-center py-5 px-xl-5">
+                <div className="col-md-6 col-12 py-5">
+                    <div className="text-center mb-2 pb-2">
+                        <h2 className="section-title px-5 mb-3"><span className="bg-secondary px-2">Stay Updated</span></h2>
+                        <p>Amet lorem at rebum amet dolores. Elitr lorem dolor sed amet diam labore at justo ipsum eirmod duo labore labore.</p>
+                    </div>
+                    <div className="text-center mb-2 pb-2">                      
+                        {/* <p>You have subscribed!</p> */}
+                        <p> Thank you. You have subscribed successfully with {emailadd}</p>
+                    </div>       
+                </div>
+            </div>
+        </div>
+        }
+
+        </>
+    )
+}
+
 export default Subscription;
