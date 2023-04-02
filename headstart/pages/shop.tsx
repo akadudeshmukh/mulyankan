7d5e7ba6156742508fc6e5771d3d0060f35e2d83
Tej Mehta <tmehta@horizontal.com>
2023-04-02 19:19:34 +0530

    added breadcrumb

--- headstart/pages/shop.tsx
+++ headstart/pages/shop.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
+import Breadcrumb from "@/components/Breadcrumb/Breadcrumb";
 import { Filters } from "@/components/PageComponents/ShopFilters";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,6 +14,7 @@ export default function Shop() {
       <Layout title="Shop-Barbarik">
         {/* Breadcrumb */}
         {/* Filters */}
+        <Breadcrumb page="Shop"></Breadcrumb>
         <Filters />
         {/* Product List */}
         {/* Pagination */}
