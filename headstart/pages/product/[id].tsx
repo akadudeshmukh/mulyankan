3026590e9f958cd3ead7729ea5166e652c410cc9
vke-csekar <93593553+vke-csekar@users.noreply.github.com>
2023-04-02 18:00:04 +0530

    Product Details

--- headstart/pages/product/[id].tsx
+++ headstart/pages/product/[id].tsx
@@ -1,5 +1,4 @@
 import AddToCart from '@/components/ProductDetails/AddToCart'
-import Image from 'next/image'
 import { Product } from '@/types/product'
 import Layout from '@/components/Layout/Layout'
 import { Inter } from '@next/font/google'
@@ -9,9 +8,6 @@ import ProductInformation from '@/components/ProductDetails/ProductInformation'
 import { loremIpsum } from 'lorem-ipsum';
 
 import ProductTabs from '@/components/ProductDetails/ProductTabs'
-import exp from 'constants'
-
-const inter = Inter({ subsets: ['latin'] })
 
 type Props = {
   product: Product,
@@ -19,78 +15,53 @@ type Props = {
   longDescription: string
 };
 
-export default function ProductDetails(props: Props) {  
+export default function ProductDetails(props: Props) {
   var images: string[] = [];
-  if(props.product.images){
-    images = props.product.images;    
-  }  
-  const pageTitle = props.product.title + " - Product";  
-  return (    
+  if (props.product.images) {
+    images = props.product.images;
+  }
+  const pageTitle = props.product.title + " - Product";
+  return (
     <Layout title={pageTitle}>
       <Breadcrumb category={props.product.category ?? ""} page={props.product.title ?? ""} />
       <div className='container-fluid py-5'>
         <div className='row px-xl-5'>
           <div className='col-lg-5 pb-5'>
-            <ProductImageCarousel images={images}/>                    
+            <ProductImageCarousel images={images} />
           </div>
           <div className='col-lg-7 pb-5'>
-            <ProductInformation product={props.product} productDescription={props.shortDescription}/> 
-            <AddToCart product={props.product}></AddToCart>         
+            <ProductInformation product={props.product} productDescription={props.shortDescription} />
+            <AddToCart product={props.product}></AddToCart>
           </div>
         </div>
         <div className='row px-xl-5'>
           <div className='col'>
-            <ProductTabs  product={props.product} productDescription={props.longDescription} />
+            <ProductTabs product={props.product} productDescription={props.longDescription} />
           </div>
         </div>
-      </div>            
+      </div>
     </Layout>
   )
 }
 
-// export async function getStaticPaths() {
-//   const response = await fetch('https://dummyjson.com/products');
-//   var productsJson = await response.json();
-//   var products = productsJson.products;  
-//   const paths = products.map((product: any, i: number) => ({
-//     params: {id: product.id.toString()}
-//   }));
-//   return { paths, fallback: false };
-// }
-
-export async function getServerSideProps(context: any){
-  const response = await fetch('http://localhost:3000/api/product/' + context.params.id);
-  var product: Product = await response.json();   
-    if(product && product.id) {
-      return {
-        props:{
-          product: product,
-          shortDescription: shortDescription(),
-          longDescription: longDescription()
-        }
-      };
-    }  
+export async function getServerSideProps(context: any) {
+  const response = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT + '/api/product/' + context.params.id);
+  var product: Product = await response.json();
+  if (product && product.id) {
     return {
-      notFound: true,
+      props: {
+        product: product,
+        shortDescription: shortDescription(),
+        longDescription: longDescription()
+      }
     };
+  }
+  return {
+    notFound: true,
+  };
 }
 
-// export async function getStaticProps(context: any){  
-//   var product: Product = {};
-//   if(context.params && context.params.id){    
-//     const response = await fetch('http://localhost:3000/api/product/' + context.params.id);
-//     product = await response.json();          
-//   }  
-//   return {
-//     props:{
-//       product: product,
-//       shortDescription: shortDescription(),
-//       longDescription: longDescription()
-//     }
-//   };
-// }
-
-function shortDescription(){  
+function shortDescription() {
   const description = loremIpsum({
     count: 5,
     format: "plain",
@@ -100,12 +71,12 @@ function shortDescription(){
     sentenceLowerBound: 5,
     sentenceUpperBound: 15,
     suffix: "\n",
-    units: "sentences"    
-});
-return description;
+    units: "sentences"
+  });
+  return description;
 }
 
-function longDescription(){  
+function longDescription() {
   const description = loremIpsum({
     count: 5,
     format: "html",
@@ -115,7 +86,7 @@ function longDescription(){
     sentenceLowerBound: 5,
     sentenceUpperBound: 15,
     suffix: "\n",
-    units: "paragraphs"    
-});
-return description;
+    units: "paragraphs"
+  });
+  return description;
 }
--- headstart/pages/shopping-cart.tsx
+++ headstart/pages/shopping-cart.tsx
@@ -6,34 +6,34 @@ import CartLines from "@/components/ShoppingCart/CartLines";
 
 type Props = {
    cart: AddToCartResponse
- };
+};
 
-export default function ShoppingCart(props: Props) {  
-   return(
-    <Layout title="Shopping Cart">
-    <Breadcrumb page="Shopping Cart" />
-    <CartLines cart={props.cart} />
-    </Layout>
+export default function ShoppingCart(props: Props) {
+   return (
+      <Layout title="Shopping Cart">
+         <Breadcrumb page="Shopping Cart" />
+         <CartLines cart={props.cart} />
+      </Layout>
    )
 }
 
-export async function getServerSideProps(context: any){   
-   if(context.req && context.req.cookies && context.req.cookies.Cart){      
-      var cookieProducts: AddToCartResponse = JSON.parse(context.req.cookies.Cart);  
-      if(cookieProducts && cookieProducts.id && cookieProducts.products) {         
-         for(let i = 0; i < cookieProducts.products.length; i++){
-            const response = await fetch('http://localhost:3000/api/product/' + cookieProducts.products[i].id);
-            var product: Product = await response.json();  
+export async function getServerSideProps(context: any) {
+   if (context.req && context.req.cookies && context.req.cookies.Cart) {
+      var cookieProducts: AddToCartResponse = JSON.parse(context.req.cookies.Cart);
+      if (cookieProducts && cookieProducts.id && cookieProducts.products) {
+         for (let i = 0; i < cookieProducts.products.length; i++) {
+            const response = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT + '/api/product/' + cookieProducts.products[i].id);
+            var product: Product = await response.json();
             cookieProducts.products[i].image = product.images && product.images[0];
-         }         
+         }
          return {
-            props:{
-              cart: cookieProducts
+            props: {
+               cart: cookieProducts
             }
-          };
-        } 
-   }  
-     return {
-       notFound: true,
-     };
- }
+         };
+      }
+   }
+   return {
+      notFound: true,
+   };
+}
